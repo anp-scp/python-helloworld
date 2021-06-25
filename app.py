a4ecd6e0f1f48a2ca705b4e4807583b90cc65f82
Anupam Sharma <b15cs154@cit.ac.in>
2021-06-25 23:26:50 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import jsonify
+import logging as log
+app = Flask(__name__)
+log.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", filename="app.log", level=log.DEBUG)
+
+@app.route("/")
+def hello():
+    endpoint = "/"
+    log.debug("The endpoint: '" + endpoint + "' was reached")
+    return "Hello World!"
+
+@app.route("/status")
+def status():
+    endpoint = "/status"
+    log.debug("The endpoint: '" + endpoint + "' was reached")
+    data = {"result": "OK - healthy"}
+    return jsonify(data), 200
+
+@app.route("/metrics")
+def metrics():
+    endpoint = "/metrics"
+    log.debug("The endpoint: '" + endpoint + "' was reached")
+    data = {"UserCount": 140, "UserCountActive": 23}
+    return jsonify(data), 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
